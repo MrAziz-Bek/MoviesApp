ad34caa93f25bbebe3feacdd1bd407496379c70d
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-11-09 23:13:55 +0500

    Two-way data binding in razor pages

--- MoviesApp/MoviesApp/Pages/AddMovie.cshtml.cs
+++ MoviesApp/MoviesApp/Pages/AddMovie.cshtml.cs
@@ -1,11 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MoviesApp.Pages
 {
     public class AddMovieModel : PageModel
     {
+        [BindProperty]
+        public string Title { get; set; }
+
+        [BindProperty]
+        public int Rate { get; set; }
+
+        [BindProperty]
+        public string Description { get; set; }
+
         public void OnGet()
         {
+            Title = "Welcome";
+        }
+
+        public IActionResult OnPost()
+        {
+            string value = $"{Title} - {Rate} - {Description}";
+            Console.WriteLine(value);
+            
+            return Page();
+            // return Redirect("Movies");
         }
     }
 }
